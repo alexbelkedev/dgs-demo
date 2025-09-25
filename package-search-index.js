4b65d5bbf3e613120a1d0080ecb1db317501af4e
alexbelkedev <alexbelkedev@users.noreply.github.com>
2025-09-25 16:06:09 +0000

    docs: publish JavaDoc (auto) 0b9e6c86b8fb49c44226ca09108a21e69ca6ec42

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.dgs.demo"},{"l":"com.example.dgs.demo.books.infrastructure.graphql"},{"l":"com.example.dgs.demo.books.model"},{"l":"com.example.dgs.demo.config"}];updateSearchResults();
